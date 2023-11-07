00945a692bed94e1a6018ef472eba2ef3845735c
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-11-06 22:38:29 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Next.js Mail',
+  description: 'An email client template using the Next.js App Router.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className="bg-white dark:bg-gray-950 text-black dark:text-white"
+    >
+      <body className={GeistSans.className}>{children}</body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tailwindcss-react-aria-components')],
+};
+export default config;
